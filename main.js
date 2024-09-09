e0dba8ecea08c9c7644c09a349f7228b212c98f1
jitmauyeheskielhuya <jitmauyeheskiel@mail.com>
2024-09-10 01:44:24 +0900

    pertengahan belajar

---
+++ main.js
@@ -0,0 +1,14 @@
+// alert('heyjo')
+
+// prompt('masukan usia anda')
+
+// var usia = 30
+// const x = 1
+// let y = 2
+
+// let usia = 30
+// usia = 35
+// console.log('heyhooo usia kamu adalah ' + usia)
+// alert('usia kamu adalah ' + usia)
+
+prompt('berapa usia kamu..?')
